5454ed08e237563bfdf5807f477ab42e587913d2
Jason Moore <moorejbm@gmail.com>
2021-02-01 15:33:37 -0600

    CompletedExercise

---
+++ BasicClassesExercise1/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClassesExercise1
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public Car(string makeInput, string modleInput, int year)
+        {
+            Make = makeInput;
+            Model = modleInput;
+            Year = year;             
+                
+        }
+        
+        //fields
+        //Properties
+        //Methods 
+
+        // Create a Make property of type string that is public 
+        public string Make { get; set; } //get = read set = weight 
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ BasicClassesExercise1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+    namespace BasicClassesExercise1
+    {
+        class Program
+        {
+
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Honda";
+            myCar.Model = "Accord 2.0t";
+            myCar.Year = 2018;
+
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2016
+            };
+
+            var chevy = new Car("Chevy", "Impala", 2001);
+
+            var carList = new List<Car>() { myCar, mazda, chevy};
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+
+            }
+
+            Console.WriteLine(carList.Capacity);
+
+            }
+
+            
+
+
+        
+    
+    
+    }
+   
+}
